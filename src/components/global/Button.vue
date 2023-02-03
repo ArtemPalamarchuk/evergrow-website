f5073b5b2219f36f561d3b4a91f38186c6388b51
artemandreevich <foks992@gmail.com>
2023-02-03 14:11:55 +0200

    add Button variants

--- src/components/global/Button.vue
+++ src/components/global/Button.vue
@@ -1,20 +1,46 @@
-<script setup lang="ts">
-interface ButtonProps {
-  text: string,
+<script lang="ts">
+import { defineComponent } from "vue";
+import type { PropType } from "vue";
+
+enum ButtonVariants {
+  primary = 'primary',
+  black = 'black',
+  white = 'white',
 }
 
-const {text} = defineProps<ButtonProps>()
+export default defineComponent({
+  props: {
+    variant: {
+      type: String as PropType<ButtonVariants>,
+      default: 'primary'
+    },
+    text: String,
+    img: String
+  },
+  computed: {
+    classes() {
+      const default_styles = 'bg-primary text-white'
+
+      if (this.variant === ButtonVariants.primary) return default_styles
+      if (this.variant === ButtonVariants.black) return 'bg-primary-black text-white'
+      if (this.variant === ButtonVariants.white) return 'bg-white text-primary-black'
+
+      return default_styles
+    }
+  },
+})
 </script>
 
 <template>
-  <button>
+  <button :class="classes">
     {{ text }}
+    <img v-if="img" :src="img"/>
   </button>
 </template>
 
 <style scoped>
   button {
-    @apply flex items-center rounded-md py-1.5 px-6 cursor-pointer bg-primary text-md-i text-white whitespace-nowrap
-    sm-l:text-lg-i
+    @apply flex items-center rounded-[60px] cursor-pointer whitespace-nowrap
+           text-xmd-i font-urbanist-sb
   }
 </style>
