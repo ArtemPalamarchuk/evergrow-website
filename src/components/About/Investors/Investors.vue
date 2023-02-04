96dd88e7df3cc3e4653e76282f9c55c9c0dd55e2
artemandreevich <foks992@gmail.com>
2023-02-05 01:02:41 +0200

    pretty sponsors

--- src/components/About/Investors/Investors.vue
+++ src/components/About/Investors/Investors.vue
@@ -1,32 +1,33 @@
-<script setup lang="ts">
+<script lang="ts">
 import { xyz, congruent, firstRound, journey, garuda } from '@/assets/images'
 import Person from '../Investors/Person.vue'
 import { investorsContent } from "@/data";
+
+export default {
+  components: {Person},
+  setup() {
+    return {
+      investorsContent,
+      sponsors_images: [xyz, congruent, firstRound, journey, garuda]
+    }
+  }
+}
 </script>
 
 <template>
   <section class="investors full-width-filled">
     <div class="max-w-[1440px] mx-auto">
-      <h2 class="title">Our investors</h2>
+      <h2 class="text-white text-sm-h sm-l:text-md-h-tab md:text-lg-h text-center mb-8">Our investors</h2>
+
       <div class="images-container">
-        <div class="image-container rounded-tl-[1.5rem] rounded-lg sm-l:rounded-lg basis-[calc(100%/3-5.33px)] sm-l:basis-1/5">
-          <img :src="xyz" alt="xyz" loading="lazy">
-        </div>
-        <div class="image-container basis-[calc(100%/3-5.33px)] rounded-lg sm-l:rounded-lg sm-l:basis-1/5"><img
-          :src="congruent" alt="congruent" loading="lazy">
-        </div>
-        <div class="image-container basis-[calc(100%/3-5.33px)] rounded-lg sm-l:rounded-lg sm-l:basis-1/5"><img
-          :src="firstRound" alt="firstRound" loading="lazy">
-        </div>
-        <div class="image-container rounded-bl-[1.5rem] rounded-lg sm-l:rounded-lg basis-[calc(100%/2-4px)] sm-l:basis-1/5">
-          <img :src="garuda" alt="journey" loading="lazy">
-        </div>
-        <div class="image-container rounded-br-[1.5rem] rounded-lg sm-l:rounded-lg basis-[calc(100%/2-4px)] sm-l:basis-1/5">
-          <img :src="journey" alt="garuda" loading="lazy">
+        <div v-for="image in sponsors_images"
+             class="w-full h-[100px] flex items-center justify-center basis-[calc(100%/3-5.33px)] sm-l:basis-1/5">
+          <img :src="image" loading="lazy"/>
         </div>
       </div>
-      <div class="person-list">
-        <Person v-for="person in investorsContent" :person="person"/>
+
+      <div class="person-list grid grid-cols-2 md:grid-cols-3 gap-4 sm:gap-6 md:gap-8 md:ml-[120px]">
+        <person v-for="person in investorsContent" :person="person"/>
       </div>
     </div>
   </section>
@@ -34,32 +35,22 @@ import { investorsContent } from "@/data";
 </template>
 
 <style scoped>
-.investors {
-  @apply bg-primary-black py-12 px-4
-  lg:px-[120px] lg:py-40
-  md:px-10 md:py-12
-}
+  .investors {
+    @apply bg-primary-black
+    px-4  md:px-10 lg:px-[120px]
+    py-12 md:py-12 lg:py-40
+  }
 
-.title {
-  @apply text-white text-sm-h sm-l:text-md-h-tab md:text-lg-h text-center mb-8;
-}
+  .images-container {
+    @apply flex bg-[#1d3032] justify-center
+    flex-wrap sm-l:flex-nowrap
+    gap-2 md:gap-3 lg:gap-8
+    mb-10 lg:mb-16
+    px-2 sm-l:px-3 md:px-8 lg:px-[70px]
+    py-5
+  }
 
-.images-container {
-  @apply flex flex-wrap sm-l:flex-nowrap gap-2 md:gap-3 lg:gap-8 bg-[#1d3032] mb-10 lg:mb-16 px-2 py-5 rounded-2xl
-   lg:px-[70px]
-   md:px-8
-   sm-l:px-3
-}
-
-.image-container {
-  @apply w-full h-[100px] flex items-center justify-center md:rounded-2xl ;
-}
-
-.person-list {
-  @apply grid grid-cols-2 md:grid-cols-3 gap-4 sm:gap-6 md:gap-8 md:ml-[120px]
-}
-
-.person-list .card:nth-last-child(2) {
-  @apply hidden md:block
-}
+  .person-list .card:nth-last-child(2) {
+    @apply hidden md:block
+  }
 </style>
